5504f72cdf58b76fee315d3ed91d97c10d039a33
arif123654 <kmarif1432@gmail.com>
2025-09-03 01:34:37 +0600

    ratting component implementation

--- proshop/src/components/Product.jsx
+++ proshop/src/components/Product.jsx
@@ -1,25 +1,39 @@
 import { Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import Rating from "./Rating"
 
 const Product = ({ product }) => {
-  return (
+return (
     <>
-        <Card className="my-3 p-3 rounded">
-            <Link to={`/product/${product._id}`}>
-                <Card.Img src={product.image} variant="top" />
-            </Link>    
+            <Card className="my-3 p-3 rounded">
+                    <Link to={`/product/${product._id}`}>
+                            <Card.Img src={product.image} variant="top" />
+                    </Link>    
 
-            <Card.Body>
-                <Link to={`/product/${product._id}`}>
-                    <Card.Title as="div">
-                        <strong>{product.name}</strong>
-                    </Card.Title>
-                </Link>
-                <Card.Text as="h3">${product.price}</Card.Text>
-            </Card.Body>
-        </Card>
+                    <Card.Body>
+                            <Link to={`/product/${product._id}`}>
+                                    <Card.Title
+                                            as="div"
+                                            style={{
+                                                    whiteSpace: "nowrap",
+                                                    overflow: "hidden",
+                                                    textOverflow: "ellipsis",
+                                                    display: "block",
+                                            }}
+                                            title={product.name}
+                                    >
+                                            <strong>{product.name}</strong>
+                                    </Card.Title>
+                            </Link>
+                            <Card.Text as="div">
+                                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                            </Card.Text> 
+
+                            <Card.Text as="h3">${product.price}</Card.Text>
+                    </Card.Body>
+            </Card>
     </>
-  )
+)
 }
 
 export default Product
