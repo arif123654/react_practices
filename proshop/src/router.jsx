383ddafc6a4cf10e390170fff84ccfba592a3dac
arif123654 <kmarif1432@gmail.com>
2025-09-03 01:48:29 +0600

    product details page

--- proshop/src/router.jsx
+++ proshop/src/router.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 import App from "./App";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
+        <Route path="product/:id" element={<ProductPage/>} />
     </Route>
   )
 )
